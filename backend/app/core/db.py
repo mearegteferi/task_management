44cdf468b701fedbd723770ceebb6800b4eb7141
Meareg Teferi <mearegbuna@gmail.com>
2026-02-02 21:50:50 +0300

    remove boilerplate code from project

--- backend/app/core/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-
-from app.core.config import settings
-
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
-
-class Base(DeclarativeBase):
-    pass
